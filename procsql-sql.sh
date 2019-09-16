4e15902eb8f1b7efa26a930394366fa48e26687e
amirga74 <54889412+amirga74@users.noreply.github.com>
2019-09-16 09:22:48 +0300

    Update procsql-sql.sh

--- procsql-sql.sh
+++ procsql-sql.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 sqlplus -s amir/amir@localhost << EOF
-
-whenever sqlerror exit sql.sqlcode;
-set echo off 
-set heading off
+REATE OR REPLACE PROCEDURE number_of_users AS
+                total_users NUMBER;
+                WHENEVER SQLERROR EXIT SQL.SQLCODE
+              BEGIN
+              SELECT COUNT(*) into total_users FROM all_users;
+              END;
 
 @procsql.sql
 
