9ec67e306edad5389db7f8a1d0da9c67f46b7853
amirga74 <54889412+amirga74@users.noreply.github.com>
2019-09-16 09:36:11 +0300

    Create sqlerror.sh

---
+++ sqlerror.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sqlplus -s fail/fil@localhost << EOF
+REATE OR REPLACE PROCEDURE number_of_users AS
+                total_users NUMBER;
+                WHENEVER SQLERROR EXIT SQL.SQLCODE
+              BEGIN
+              SELECT COUNT(*) into total_users FROM all_users;
+              END;
+whenever sqlerror exit sql.sqlcode;
+set echo off 
+set heading off
+
+@procsql.sql
+
+exit;
+EOF
